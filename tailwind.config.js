ec588894e55d66a8731ac47ea75fbb512ac648ae
AthenaNavard <149033151+AthenaNavard@users.noreply.github.com>
2024-08-02 12:10:29 +0330

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+export default { 
   content: ["./*.html"],
   darkMode: 'class',
   theme: {
     extend: {
       fontFamily:{ IRANSans:['IRANSans']},
-      backgroundImage: {
-        'card-image': "url('/public/imges/Card (1).png')",
-        'card-image2': "url('/public/imges/Rectangle 1313.png')",
-        'card-image3': "url('/public/imges/Card.png')",
-        'card-left': "url('/public/imges/left-card-vi.png')",
-        'card-right': "url('/public/imges/top-card.png')",
-        'back': "url('/public/imges/pri-b.png')"
-
-      },
 
     },
   },
